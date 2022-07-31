b87d59d1d7428bddd7ffa788403b695276788da0
Adam Zawierucha <zawiedev@gmail.com>
2022-07-31 16:15:45 -0700

    remove swap

--- src/feisel.rs
+++ src/feisel.rs
@@ -4,20 +4,20 @@ pub fn encrypt(data: u64, key: u64) -> u64 {
     let (mut left, mut right) = split(data);
 
     for round in 0..ROUNDS {
-        (left, right) = swap(left ^ prng(right, subkey(key, round)), right)
+        (left, right) = (right, left ^ prng(right, subkey(key, round)))
     }
 
-    combine(swap(left, right))
+    combine((right, left))
 }
 
 pub fn decrypt(cipher: u64, key: u64) -> u64 {
     let (mut left, mut right) = split(cipher);
 
     for round in (0..ROUNDS).rev() {
-        (left, right) = swap(left ^ prng(right, subkey(key, round)), right)
+        (left, right) = (right, left ^ prng(right, subkey(key, round)))
     }
 
-    combine(swap(left, right))
+    combine((right, left))
 }
 
 /*
@@ -37,10 +37,6 @@ fn split(x: u64) -> (u32, u32) {
 
 fn combine((l, r): (u32, u32)) -> u64 {
     ((l as u64) << 32) + (r as u64)
-}
-
-fn swap(a: u32, b:u32) -> (u32, u32) {
-    (b, a)
 }
 
 fn subkey(key: u64, idx: u8) -> u8 {
