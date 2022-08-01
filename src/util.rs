47bd7be3978dd21cb69703a5b9788c16a815923a
Adam Zawierucha <zawiedev@gmail.com>
2022-08-01 08:03:55 -0700

    make word_to_bytes public

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,5 @@
 
-fn word_to_bytes(word: u64) -> [u8; 8] {
+pub fn word_to_bytes(word: u64) -> [u8; 8] {
     let mut arr: [u8; 8] = [0,0,0,0,0,0,0,0];
     for i in 1..=8 {
         arr[i-1] = (word >> (8 - i)*8) as u8
