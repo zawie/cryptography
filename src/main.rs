b537f5af291d565d0e547cdbda4d5918d961915a
Adam Zawierucha <zawiedev@gmail.com>
2022-07-31 23:30:50 -0700

    cat encryption

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,26 @@ use crypto::nonce::decrypt_stream;
 fn main() {
     let key = 0x2ab56ef8dc104566;
 
-    let mut data_read = File::open("./resources/data.txt").unwrap();
+    let input_path = "./resources/cat.webp";
+    let encrypt_path = "./resources/encrypted_data";
+    let output_path = "./resources/output.webp";
+
+    let mut data_read = File::open(input_path).unwrap();
 
     let mut encrypted_write = OpenOptions::new()
         .write(true)
         .append(true)
-        .open("./resources/encrypted_data.txt")
+        .open(encrypt_path)
         .unwrap();
 
     encrypt_stream(&mut data_read, &mut encrypted_write, key);
 
-    let mut encrypted_read = File::open("./resources/encrypted_data.txt").unwrap();
+    let mut encrypted_read = File::open(encrypt_path).unwrap();
 
     let mut decrypted_write = OpenOptions::new()
         .write(true)
         .append(true)
-        .open("./resources/decrypted_data.txt")
+        .open(output_path)
         .unwrap();
 
     decrypt_stream(&mut encrypted_read, &mut decrypted_write, key);
